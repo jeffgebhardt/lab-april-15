61cd40b9b505fd7c0c18bcc269c97e5a6e018dc2
Jeffrey Gebhardt <jeff@gebbey.com>
2016-04-15 13:52:22 -0700

    added function three

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 function sum(numOne, numTwo){
   var answer = numOne + numTwo;
-  var output = '\"The sum of ' + numOne + ' and ' + numTwo + ' is ' + answer + '\"';
-  return output;
+  //console.log('\"The sum of ' + numOne + ' and ' + numTwo + ' is ' + answer + '\"');
+  return answer;
 };
 
-function mulitply(numOne, numTwo){
+function multiply(numOne, numTwo){
   var answer = numOne * numTwo;
-  var output = '\"The product of ' + numOne + ' and ' + numTwo + ' is ' + answer + '\"';
-  return output;
+  //console.log('\"The product of ' + numOne + ' and ' + numTwo + ' is ' + answer + '\"');
+  return answer;
 };
+
+function sumAndMultiply(numOne, numTwo, numThree){
+  var answer = [];
+
+  var x = sum(numOne, numTwo);
+  answer.push(sum(x, numThree));
+  var y = multiply(numOne, numTwo);
+  answer.push(multiply(y, numThree));
+
+  console.log('\"' + numOne + ' and ' + numTwo + ' and ' + numThree + ' sum to ' + answer[0] + '\"');
+  console.log('\"The  numbers ' + numOne + ' and ' + numTwo + ' and ' + numThree + ' have a product of ' + answer[1] + '\"');
+}
